88149e74e041727c8d2873af05240627c302e17e
xuanwuGG <xuanwuGG@github.com>
2024-05-10 22:28:51 +0800

    最终版（版本2）

--- MC/frontEnd.py
+++ MC/frontEnd.py
@@ -5,40 +5,62 @@ import time
 from main import NMF_get_predictions, SVD_get_predictions
 from surprise import accuracy
 import plotly.graph_objs as go
+import pandas as pd
+import base64
+import io
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.H1('Machine Learning Working Design', style={'textAlign': 'center'}),
+    html.Label('adjust epochs:'),
+    dcc.Slider(
+        id='slider',
+        min=0,
+        max=20,
+        step=1,
+    ),
+    html.Div(id='slider-output-container'),
     dcc.Dropdown(
         id='dropdown',
         options=[
             {'label': 'SVD', 'value': 'SVD'},
             {'label': 'NMF', 'value': 'NMF'}
         ],
-        value=None,
         style={'width': '50%', 'margin': 'auto'}
     ),
     html.Div([
         dcc.Input(id='start', type='number', placeholder='Start index'),
-        dcc.Input(id='end', type='number', placeholder='End index')
-    ], style={'display': 'flex', 'justify-content': 'center', 'margin-top': '120px'}), 
+        dcc.Input(id='end', type='number', placeholder='End index'),
+        html.Button('Submit', id='submit_button', n_clicks=0)
+    ], style={'display': 'flex', 'justify-content': 'center', 'margin-top': '80px'}), 
+    dcc.Markdown('''
+                 ## 须知  
+                    1. 网站源程序**未进行任何健壮性相关检测**，请按照步骤操作  
+                    2. 请先选择算法，再输入编号范围  
+                    3. 网站加载时，**不建议**再进行任何操作  
+                    4. 数据集应放置在与源程序同一目录下
+                 '''),
     html.Div(id='output-container')
 ])
 
+
 @app.callback(
     dash.dependencies.Output('output-container', 'children'),
-    [dash.dependencies.Input('dropdown', 'value'),
-     dash.dependencies.Input('start', 'value'),
-     dash.dependencies.Input('end', 'value')])
-def update_output(value, start, end):
-    if value is None or start is None or end is None:
-        return html.Div()
-    else:
-        if value == 'SVD':
-            predictions = SVD_get_predictions()
-        elif value == 'NMF':
-            predictions = NMF_get_predictions()
+    [dash.dependencies.Input('submit_button', 'n_clicks'),
+     dash.dependencies.Input('slider', 'value')],
+    [dash.dependencies.State('dropdown', 'value'),
+     dash.dependencies.State('start', 'value'),
+     dash.dependencies.State('end', 'value')])
+def update_output(n_clicks,slider_value,value, start, end):
+   if n_clicks > 0:
+        if value is None or start is None or end is None:
+            return html.Div()
+        else:
+            if value == 'SVD':
+                predictions = SVD_get_predictions(n_epochs=int(slider_value))
+            elif value == 'NMF':
+                predictions = NMF_get_predictions(n_epochs=int(slider_value))
         # 过滤出编号在指定区间的预测结果
         filtered_predictions = predictions[start:end]
         # 提取真实分和预测分
@@ -63,7 +85,9 @@ def update_output(value, start, end):
         return html.Div([
             dcc.Graph(figure=figure),
             dcc.Graph(figure=histogram)
-        ])
+        ]) 
+
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- MC/main.py
+++ MC/main.py
@@ -1,4 +1,7 @@
 import os
+import base64
+import csv
+import io
 import numpy as np  
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
@@ -6,28 +9,12 @@ from surprise import SVD, Dataset, Reader, accuracy,NMF
 from surprise.model_selection import cross_validate, train_test_split,GridSearchCV, RandomizedSearchCV
 import matplotlib.pyplot as plt
 
-dataset_path = os.path.join(os.getcwd(),'MC\\archive')
-
-# 读取并处理评分数据
-ratings_file_path = os.path.join(dataset_path, 'BX-Book-Ratings.csv')
-ratings_df = pd.read_csv(ratings_file_path, sep=';', header=0,encoding='iso-8859-1')
-ratings_df.columns = ['user_id', 'book_id', 'rating']
-ratings_df['rating'] = ratings_df['rating'].apply(lambda x: float(x))
-
-
 
 # 读取并处理书籍数据
-books_file_path = os.path.join(dataset_path, 'BX-Books.csv')
-books_df = pd.read_csv(books_file_path, sep=';', header=0, on_bad_lines='skip',encoding='iso-8859-1',dtype={3:str})
-books_df.columns = ['book_id', 'title', 'author', 'year_of_publication', 'publisher', 'image_url_s', 'image_url_m', 'image_url_l']
-
-mask = ratings_df['book_id'].isin(books_df['book_id'])
-ratings_df = ratings_df[mask]
-
-# 使用 LabelEncoder 对书籍 ID 进行编码
-le = LabelEncoder()
-books_df['book_id'] = le.fit_transform(books_df['book_id'])
-ratings_df['book_id'] = le.transform(ratings_df['book_id'])
+dir=os.path.dirname(os.path.abspath(__file__))
+path=os.path.join(dir,'BX-Book-Ratings.csv')
+ratings_df=pd.read_csv(path, sep=';', header=0, on_bad_lines='skip',encoding='iso-8859-1',dtype={3:str})
+ratings_df.columns = ['user_id', 'book_id', 'rating']
 
 
 reader = Reader(rating_scale=(0,10))
@@ -48,15 +35,15 @@ data = Dataset.load_from_df(ratings_df[['user_id', 'book_id', 'rating']], reader
 # 分割数据集
 trainset, testset = train_test_split(data, test_size=.25)
 
-def SVD_get_predictions():
-    best_params = {'n_epochs': 12, 'lr_all': 0.005, 'reg_all': 0.2}
+def SVD_get_predictions(n_epochs=12, lr_all=0.005, reg_all=0.2):
+    best_params = {'n_epochs': n_epochs, 'lr_all': lr_all, 'reg_all': reg_all}
     algo = SVD(**best_params)
     algo.fit(trainset)
     predictions = algo.test(testset)
     return predictions
 
-def NMF_get_predictions():
-    best_params = {'n_epochs': 25, 'n_factors': 80}  # 假设的最佳参数
+def NMF_get_predictions(n_epochs=25, n_factors=80):
+    best_params = {'n_epochs': n_epochs, 'n_factors': n_factors}
     algo = NMF(**best_params)
     algo.fit(trainset)
     predictions = algo.test(testset)
@@ -79,16 +66,3 @@ def NMF_get_predictions():
 
 
 
-# 展示预测结果
-# for uid, iid, true_r, est, _ in predictions[:10]:
-#     print(f'user {uid} for item {iid}: actual rating {true_r}, estimated rating {est}')
-
-
-# 计算误差
-# errors = [true_r - est for uid, iid, true_r, est, _ in predictions]
-# 绘制误差分布图
-# plt.hist(errors, bins='auto')
-# plt.title('Distribution of prediction errors')
-# plt.xlabel('Error')
-# plt.ylabel('Frequency')
-# plt.show()
